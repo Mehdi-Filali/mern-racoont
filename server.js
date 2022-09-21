c037de6f61556014bd48fcf6fba7db32aa7ce093
Filali Mehdi <aniq.filali.mehdi@gmail.com>
2022-09-21 11:31:30 +0200

    deployment

--- server.js
+++ server.js
@@ -41,6 +41,14 @@ app.get("/jwtid", requireAuth, (req, res) => {
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 
+//production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //server
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
